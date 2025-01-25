41be115e6cb5c3d3d63ea6f69c8c5e6de6dba1ec
Hichem Fantar <fantar.hichem@gmail.com>
2025-01-25 22:14:03 +0100

    fix: update blog and news index components to use MdxFile type and improve title rendering

--- components/blog-index.tsx
+++ components/blog-index.tsx
@@ -1,9 +1,11 @@
-// @ts-nocheck
+import type { MdxFile } from "nextra";
 import { Link } from "nextra-theme-docs";
 import { getPagesUnderRoute } from "nextra/context";
 
 export default function BlogIndex({ more = "Read more" }) {
-  return getPagesUnderRoute("/blog").map((page) => {
+  const pageMap = getPagesUnderRoute(`/blog`) as unknown as MdxFile[];
+
+  return pageMap.map((page) => {
     return (
       <div key={page.route} className="mb-10">
         <h3>
@@ -12,7 +14,7 @@ export default function BlogIndex({ more = "Read more" }) {
             style={{ color: "inherit", textDecoration: "none" }}
             className="block font-semibold mt-8 text-2xl "
           >
-            {page.meta?.title || page.frontMatter?.title || page.name}
+            {page.frontMatter?.title || page.name}
           </Link>
         </h3>
         <p className="opacity-80 mt-6 leading-7">
